6fc4beba22373f8dd0ae51ad97e1ea9d447d8656
kgrac <kasandragraciano05@gmail.com>
2023-09-20 23:43:26 -0500

    initial commit

--- queries.sql
+++ queries.sql
@@ -1,5 +1,5 @@
 -- Part 1: Test it with SQL
-
+-- int id, String employer, String name, String skills
 -- Part 2: Test it with SQL
 
 -- Part 3: Test it with SQL
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,28 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
+
+    public String description;
+
+    @Size(max = 150)
+    public String getDescription() {
+        return description;
+    }
+
+    public Skill(String description) {
+        this.description = description;
+    }
+
+    public Skill() {
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 
 }
